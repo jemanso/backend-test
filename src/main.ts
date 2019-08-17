6833b35c76e354e6c0a09e003051d6f17937131b
Eduardo Manso <manso@manso.com.br>
2019-08-17 13:04:53 -0300

    implement Winston logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { connect } from "mongoose"
 import "reflect-metadata"
 
-import { GQL_PORT, MONGODB_URI } from "./constants"
+import { GQL_PORT, MONGODB_URI, SERVER_ID } from "./constants"
 import { createServer } from "./graphql/server"
+import { createLogger } from "./logger"
+
+const logger = createLogger(SERVER_ID)
 
 const main = async () => {
   try {
@@ -11,16 +14,16 @@ const main = async () => {
       { useNewUrlParser: true, useCreateIndex: true },
     )
   } catch (mongoConnectError) {
-    console.error(mongoConnectError)
+    logger.error(mongoConnectError)
     process.exit(1)
   }
   const server = await createServer()
 
   try {
     const { url } = await server.listen(GQL_PORT)
-    console.log(`Tickets GraphQL API Server running at ${url}`)
+    logger.info(`Tickets GraphQL API Server running at ${url}`)
   } catch (apolloError) {
-    console.error(apolloError)
+    logger.error(apolloError)
     process.exit(1)
   }
 }
--- tests-remote/ticketapi.ts
+++ tests-remote/ticketapi.ts
@@ -1,31 +1,37 @@
 import repl from "repl"
 
-import { TICKET_API_URL } from "../src/constants"
+import { SERVER_ID, TICKET_API_URL } from "../src/constants"
 import { ITicket } from "../src/entities/tickets"
+import { createLogger } from "../src/logger"
 import { omdbSearchForTitleKeywords } from "../src/remote/omdbapi"
 import { ITicketAPIResponse, TicketAPIReader } from "../src/remote/ticketapi"
 
 let responses: ITicketAPIResponse[]
+const logger = createLogger(SERVER_ID)
 
-startReading(1, 2)
+startReading(10, 20)
 // startReading(3, 500)
 
 async function startReading(pages: number = 2, limitPerPage: number = 5) {
-  console.log(`\n Starting...`)
+  logger.info("reading.started")
   responses = []
   const reader = new TicketAPIReader(TICKET_API_URL, limitPerPage)
   for (let page = 1; page <= pages; page++) {
-    responses.push(await fetchTickets(reader, page))
+    try {
+      responses.push(await fetchTickets(reader, page))
+    } catch (error) {
+      logger.info(`ERROR ${error.message}`)
+    }
   }
-  console.log(` Done!\n`)
+  logger.info("reading.done")
   showConsole()
 }
 
 async function fetchTickets(reader: TicketAPIReader, page: number): Promise<ITicketAPIResponse> {
   const reqId = `${reader.uid}.${String(page).padStart(4, "0")}`
-  console.log(` ${reqId}: fetching page ${page} limited by ${reader.limit}`)
+  logger.info(`${reqId}: fetching page ${page} limited by ${reader.limit}`)
   const response = await reader.read(page)
-  console.log(` ${reqId}: received ${(response.data || []).length} tickets`)
+  logger.info(`${reqId}: received ${(response.data || []).length} tickets`)
   return response
 }
 
