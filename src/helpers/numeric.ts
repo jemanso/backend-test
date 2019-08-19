c00c837594861bdc835f16d824e9227d25b8e86e
Eduardo Manso <manso@manso.com.br>
2019-08-19 05:45:48 -0300

    update helpers

--- src/helpers/numeric.ts
+++ src/helpers/numeric.ts
@@ -2,7 +2,7 @@ export const transformInventory = (rawInventory?: number | string | null): numbe
   return Math.max(Number(rawInventory) || 0, 0)
 }
 
-export const transformPrice = (rawPrice?: number | string): number | null => {
+export const transformPrice = (rawPrice?: number | string | null): number | null => {
   // Be careful not to convert invalid values ​​to 0 (zero)
   // "Price=0" may mean "for free", invalid values may not
   const price = Number(rawPrice)
